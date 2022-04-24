006b8b9115c136430d854aac612a6292b2177106
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2022-04-24 16:06:07 +0200

    Update MainActivity.java
    Imports

--- app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
+++ app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
@@ -3,7 +3,10 @@ package fr.fleurdelage.fleurdelage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 public class MainActivity extends AppCompatActivity {
 
     @Override
