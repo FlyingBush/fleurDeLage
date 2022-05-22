baf54f7260fed3f492d988f29de6c18525a287bf
BadTimeGiver <zyad.zekri@gmail.com>
2022-05-22 13:00:31 +0200

    Third adjust from 15 to 20

--- app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
+++ app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onTranslation(float tx, float ty, float tz) {
                 float SVt;
                 SVt= (float) Math.sqrt(Math.pow(tx,2)+Math.pow(ty,2)+Math.pow(tz,2));
-                if (SVt>15f)
+                if (SVt>20f)
                     getWindow().getDecorView().setBackgroundColor(Color.RED);
 
                 else {
