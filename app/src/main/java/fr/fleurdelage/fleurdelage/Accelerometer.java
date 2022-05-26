4458d8e932cd59b0f407c4581ac4995728f46862
BadTimeGiver <zyad.zekri@gmail.com>
2022-05-27 00:14:53 +0200

    ITS ALIVE ITS ALIVE

--- app/src/main/java/fr/fleurdelage/fleurdelage/Accelerometer.java
+++ app/src/main/java/fr/fleurdelage/fleurdelage/Accelerometer.java
@@ -11,6 +11,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.IBinder;
+import android.telephony.SmsManager;
 
 
 import androidx.annotation.NonNull;
@@ -30,17 +31,18 @@ public class Accelerometer extends Service implements SensorEventListener {
         float z=event.values[2];
         float acceleration = (float) Math.sqrt(x * x + y * y + z * z);
 
-        if(acceleration >10f){
+        if(acceleration >20f){
             /*Intent i = new Intent(Intent.ACTION_MAIN);
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             i.addCategory(Intent.CATEGORY_APP_GALLERY);
-            startActivity(i);*/
+            startActivity(i);
             Intent intent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(
                     getApplicationContext().getPackageName());
             intent.setAction(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+            startActivity(intent);*/
+            msg("0666003183");
         }
     }
 
@@ -81,5 +83,9 @@ public class Accelerometer extends Service implements SensorEventListener {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+    public void msg(String number){
+        SmsManager smsManager=SmsManager.getDefault();
+        smsManager.sendTextMessage(number, null, "send help", null, null);
     }
 }
--- app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
+++ app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
@@ -2,10 +2,14 @@ package fr.fleurdelage.fleurdelage;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission(Manifest.permission.SEND_SMS,1);
+
         if (!ForegroundRunning()){
             startForegroundService(new Intent( this, Accelerometer.class ) );
         }
@@ -30,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
             return true;}
         }
         return false;
+    }
+    private void checkPermission(String permission, int requestCode)
+    {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
+        }
+
     }
 }
 
