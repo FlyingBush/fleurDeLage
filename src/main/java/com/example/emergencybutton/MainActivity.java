d2f1eb29604ac082bfbb8190e7162e95a5e51129
Nada <75384601+nada912@users.noreply.github.com>
2022-05-27 19:06:37 +0200

    Nada

--- src/main/java/com/example/emergencybutton/MainActivity.java
+++ src/main/java/com/example/emergencybutton/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.emergencybutton;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,18 +20,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.Calendar;
 import java.util.Locale;
-import java.util.concurrent.atomic.AtomicReference;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int REQUEST_CALL = 1;
-
+    private int notificationId = 1;
 
     //Initialize variable
     FusedLocationProviderClient fusedLocationProviderClient;
-
+    double latitude;
+    double longitude;
 
     @SuppressLint("MissingPermission")
     @Override
@@ -35,9 +44,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                Location location = locationResult.getLocations().get(0);
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+
+            }
+        };
+
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
         //Initialize fusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, locationCallback, null);
 
+
+        // Set onClick Listener
+        findViewById(R.id.setBtn).setOnClickListener(this);
+        findViewById(R.id.cancelBtn).setOnClickListener(this);
 
     }
 
@@ -46,13 +78,12 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("MissingPermission")
     private void sendMessage() {
-        AtomicReference<Double> wayLatitude = new AtomicReference<>(0.0);
-        AtomicReference<Double> wayLongitude = new AtomicReference<>(0.0);
+        System.out.println(fusedLocationProviderClient);
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, loc -> {
             if (loc != null) {
-                wayLatitude.set(loc.getLatitude());
-                wayLongitude.set(loc.getLongitude());
-                System.out.println(String.format(Locale.US, "%s -- %s", wayLatitude.get(), wayLongitude.get()));
+                latitude = loc.getLatitude();
+                longitude =loc.getLongitude();
+                System.out.println(String.format(Locale.US, "%s -- %s", latitude, longitude));
 
                 // Format the associated uri
                 Uri uriGeo = Uri.parse("geo:" + loc.getLatitude() + " , " + loc.getLongitude());
@@ -99,4 +130,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    public void onClick(View view) {
+        EditText editText = findViewById(R.id.editText);
+        TimePicker timePicker = findViewById(R.id.timePicker);
+
+        // Intent
+        Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+        intent.putExtra("notificationId", notificationId);
+        intent.putExtra("message", editText.getText().toString());
+
+        // PendingIntent
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                MainActivity.this, 0, intent, PendingIntent.FLAG_IMMUTABLE
+        );
+
+        // AlarmManager
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+
+        switch (view.getId()) {
+            case R.id.setBtn:
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+
+                // Create time.
+                Calendar startTime = Calendar.getInstance();
+                startTime.set(Calendar.HOUR_OF_DAY, hour);
+                startTime.set(Calendar.MINUTE, minute);
+                startTime.set(Calendar.SECOND, 0);
+                long alarmStartTime = startTime.getTimeInMillis();
+
+                // Set Alarm
+                alarmManager.set(AlarmManager.RTC_WAKEUP, alarmStartTime, pendingIntent);
+                Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.cancelBtn:
+                alarmManager.cancel(pendingIntent);
+                Toast.makeText(this, "Canceled.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
