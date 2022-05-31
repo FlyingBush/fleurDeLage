39ca766c486eabc265d1f453a8cc6e36e1da9ed8
FlyingBush <64908554+FlyingBush@users.noreply.github.com>
2022-05-31 15:07:14 +0200

    Merge EmergencyButton à la mano

--- app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
+++ app/src/main/java/fr/fleurdelage/fleurdelage/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ActivityManager;
 import android.app.role.RoleManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -48,39 +49,31 @@ public class MainActivity extends AppCompatActivity {
                 }
             }));
 
-    public void requestRole() {
-        RoleManager roleManager = (RoleManager) getSystemService(ROLE_SERVICE);
-        if (roleManager.isRoleAvailable(RoleManager.ROLE_CALL_SCREENING)) {
-            Intent intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING);
-            roleRequest.launch(intent);
-        }
-    }
-
-    private void requestPerms() {
-        ArrayList<String> perms = new ArrayList<>();
-        if (ContextCompat.checkSelfPermission(MainActivity.this, "android.permission.READ_CONTACTS") == PackageManager.PERMISSION_DENIED) {
-            perms.add("android.permission.READ_CONTACTS");
-        }
-        if (ContextCompat.checkSelfPermission(MainActivity.this, "android.permission.READ_CONTACTS") == PackageManager.PERMISSION_DENIED) {
-            perms.add("android.permission.SEND_SMS");
-        }
-        String[] permsArray = perms.toArray(new String[0]);
-        permRequest.launch(permsArray);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         requestRole();
-        requestPerms();
+        try {
+            requestPerms();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         PhoneNumberRoomDataBase dataBase = PhoneNumberRoomDataBase.getDatabase(getApplicationContext());
         PhoneNumberDao dao = dataBase.getPhoneNumberDao();
         Executor executor = dataBase.getQueryExecutor();
         executor.execute(() -> {
             List<PhoneNumber> list = dao.getAll();
             System.out.println(list);
+        });
+        Button emergency = findViewById(R.id.Emergency);
+        emergency.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), EmergencyButton.class);
+                startActivity(i);
+            }
         });
         Button internet = findViewById(R.id.Internet);
         internet.setOnClickListener(new View.OnClickListener(){
@@ -105,6 +98,29 @@ public class MainActivity extends AppCompatActivity {
             startForegroundService(new Intent( this, Accelerometer.class ) );
         }
     }
+
+    private void requestRole() {
+        RoleManager roleManager = (RoleManager) getSystemService(ROLE_SERVICE);
+        if (roleManager.isRoleAvailable(RoleManager.ROLE_CALL_SCREENING)) {
+            Intent intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING);
+            roleRequest.launch(intent);
+        }
+    }
+
+    private void requestPerms() throws PackageManager.NameNotFoundException {
+        PackageInfo info = getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), PackageManager.GET_PERMISSIONS);
+        String[] permissions = info.requestedPermissions;//This array contains the requested permissions.
+        ArrayList<String> perms = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+                perms.add(permission);
+            }
+        }
+        String[] permsArray = perms.toArray(new String[0]);
+        permRequest.launch(permsArray);
+    }
+
     public boolean ForegroundRunning(){
         ActivityManager activityManager=(ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service:activityManager.getRunningServices(Integer.MAX_VALUE)
